ad39c713945cbafdafed47118f27100c80dbe295
romulosilvestre <romulo.silvestre@df.estudante.senai.br>
2023-03-24 12:09:12 -0300

    listagem de cursos ok

--- Contexts/AppDbContext.cs
+++ Contexts/AppDbContext.cs
@@ -12,7 +12,8 @@ public class AppDbContext : DbContext
     protected override void OnConfiguring(DbContextOptionsBuilder builder)
     {
 
-        builder.UseSqlServer("Server=LAB-F08-28;Database=EscolaDotNet;User Id=sa;Password=senai@123;TrustServerCertificate=True;");
+       // builder.UseSqlServer("Server=LAB-F08-28;Database=EscolaDotNet;User Id=sa;Password=senai@123;TrustServerCertificate=True;");
+       builder.UseSqlServer("Server=VAIAMANHECER\\SQLEXPRESS;Database=EscolaDotNet;Integrated Security=True;TrustServerCertificate=True;");
 
     }
 
